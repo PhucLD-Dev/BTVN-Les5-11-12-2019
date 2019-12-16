57878c32d4f7b3764697b746f4d0f1fedeaa3e06
Lê Đức Phúc <58520153+PhucLD-Dev@users.noreply.github.com>
2019-12-16 16:09:32 +0700

    Update Bai 10.cpp

--- Bai 10.cpp
+++ Bai 10.cpp
@@ -3,21 +3,12 @@ int main(){
 	int n;
 	printf("n= \n");
 	scanf("%d",&n) ;
-	if(n<=1){
-	printf("khong kiem duoc \n");
-}else if (n<3){
-	printf("so fibonaci gan nhat %d la: %d",n,1) ;
-}else if (n<4){
-	printf("so fibonaci gan nhat %d la: %d",n,2) ;
-} else{
 	int x1=1,x2=1,x3=2;
 	int u=0; 
-	for (int i=4;i<=n;++i) {
+	for (;x2+x3<=n;) {
 		x1=x2;
 		x2=x3;
-		x3=x2+x1;
-		if (x3<n)
-		u=x3; }
-	printf("so fibonaci gan nhat %d la: %d",n,u) ;}
+		x3=x2+x1;}
+	printf("so fibonaci gan nhat %d la: %d",n,x3) ;
 	 return 0; 
 } 
